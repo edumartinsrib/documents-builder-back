3718c43ca7905210f4b8a4df4a99ec7228651f1d
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2023-03-04 22:04:30 -0300

    feat: Adicionado husky e commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 72],
+    'footer-max-line-length': [2, 'always', 72],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', ['sentence-case']],
+    'subject-empty': [2, 'never']
+  }
+}
